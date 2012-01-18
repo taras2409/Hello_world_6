488be342549d3df8eb44c9de11c90b0ecb0a88cf
taras2409 <kolesnichenko_7@mail.ru>
2012-01-18 17:19:58 +0200

    Весь проект

---
+++ src/app/pack/helloworld6/Hello_world_6Activity.java
@@ -0,0 +1,13 @@
+package app.pack.helloworld6;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hello_world_6Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
